54592379e3f0e50fc1126622553a98a78956d4a2
jyliu11 <113219844+jyliu11@users.noreply.github.com>
2022-12-06 23:49:26 -0500

    Update models.py

--- website/models.py
+++ website/models.py
@@ -1,46 +1,21 @@
-import calendar
-import datetime
-
-def format_time(time) -> str:
-    # Convert to str with AM, PM units
-    new_time = ''
-    if len(time) == 0:
-        return new_time # Skip rest of code below, terminate early
-
-    # Specific time given by user
-    hour = int(time[:2])
-    if hour >= 1 and hour < 10:
-        new_time = time[1:] + ' AM' # remove leading 0
-    elif hour == 10 or hour == 11:
-        new_time = time + ' AM'
-    elif hour == 12:
-        new_time = time + ' PM'
-    elif hour == 0:
-        # convert 0 o clock to 12 o clock
-        new_time = '12' + time[2:] + ' AM'
-    else:
-        # convert to regular 12 hour time
-        result = hour - 12
-        if result < 10:
-            new_time = str(result)
-        else:
-            new_time = '1' + str(result - 10)
-        new_time += time[2:] + ' PM'
-    return new_time
+from . import db
+from flask_login import UserMixin
 
 
 
-def format_date(task_appt_date) -> str:
-    date_obj = None
-    if task_appt_date:
-        # Appt date is given as YYYY-MM-DD format
-        month_index = int(task_appt_date[5:7]) # int
-        month = calendar.month_name[month_index] # str
-        day = task_appt_date[8:] # str
-        year = task_appt_date[0:4] # str
-        date_obj = datetime.date(int(year), month_index, int(day)) # date object
-        day_of_week = calendar.day_name[date_obj.weekday()]
-        task_appt_date = day_of_week + ", " + month + " " + day + ", " + year
-    else: # account for optional appt dates
-        task_appt_date = None
-    return task_appt_date
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    client = db.Column(db.String(200), nullable=False)
+    appt_date = db.Column(db.String(30)) # formatted str date (ex: Tuesday, April 05, 2022)
+    appt_date_int = db.Column(db.String(20)) # YYYY-MM-DD str format
+    time = db.Column(db.String(20)) # str type, account for untimed tasks
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    status = db.Column(db.String(30), default="Not Yet Started") # Other Statuses: In Progress, Completed, Cancelled
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    tasks = db.relationship('Task')
